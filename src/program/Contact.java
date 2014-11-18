512018ff9ba19d3aa9809c333091c0f049846363
christopherdubeau <dubeauchristopher@fhda.edu>
2014-11-18 11:13:04 -0800

    Innital Commit

---
+++ src/program/Contact.java
@@ -0,0 +1,13 @@
+package program;
+
+/**
+ * @author Christopher Dubeau
+ * @author Ron Rayas
+ * @author Harpreet Setia
+ * @author Long Xu
+ * Date: 11/18/14
+ * Description:
+ */
+
+public class Contact {
+}
