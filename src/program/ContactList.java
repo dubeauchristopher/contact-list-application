858b799726b44b75d7b08deff70c7d1fe4f0f33c
LongXu <xulonggg@gmail.com>
2014-11-23 17:52:26 -0800

    Update ContactList.java

--- src/program/ContactList.java
+++ src/program/ContactList.java
@@ -8,7 +8,7 @@ import java.util.List;
  * @author Harpreet Setia
  * @author Long Xu
  * Date: 11/18/14
- * Description: This class
+ * Description: This class is designed for 
  */
 
 public class ContactList {
