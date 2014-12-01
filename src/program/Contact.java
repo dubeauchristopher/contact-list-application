45b15694776242bc5c0e19647444c2f34b9510dc
iturban7 <harpreet.setia777@gmail.com>
2014-11-30 22:15:49 -0800

    Update Contact.java

--- src/program/Contact.java
+++ src/program/Contact.java
@@ -1,4 +1,6 @@
-package program;
+
+
+import java.util.List;
 
 /**
  * @author Christopher Dubeau
@@ -6,8 +8,49 @@ package program;
  * @author Harpreet Setia
  * @author Long Xu
  * Date: 11/18/14
- * Description:
+ * Description:  This class will run the contact  menu and call methods from the contact list class to print sort and search contacts
  */
 
 public class Contact {
+
+	private String firstName; //FirstName is a data member
+	private String lastName; // LastName is a data member
+	//Eventually have to add streetaddress functionality somewhere
+	private String emailAddress;  // EmailAddress is a data member
+	private String phoneNumber; //PhoneNumber is a data member
+	private String notes;  //Notes is a data member
+	
+    public Contact(){
+// write in test code
+    }
+
+    /**
+     * This method prints an individual contact
+     * @return
+     */
+//    public Contact printContact(){
+//        return individualContact;
+//    }
+
+    /**
+     * This method prompts the user to
+     * @return
+     */
+    public void addContact(){
+
+    }
+
+    /**
+     * This method accepts values and returns a string
+     * @return
+     */
+    public String toString(){
+    	String output = "";
+    	output = firstName + " " + lastName + "/n";
+    	output+= emailAddress + "/n";
+    	output += phoneNumber + "/n";
+    	output += notes;
+        return output;
+    }
+
 }
