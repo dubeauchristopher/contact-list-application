5191e94ac8de83b7f866436221026cc120d46f63
iturban7 <harpreet.setia777@gmail.com>
2014-11-30 22:16:23 -0800

    Update ContactList.java

--- src/program/ContactList.java
+++ src/program/ContactList.java
@@ -1,4 +1,6 @@
-package program;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Christopher Dubeau
@@ -6,8 +8,76 @@ package program;
  * @author Harpreet Setia
  * @author Long Xu
  * Date: 11/18/14
- * Description:
+ * Description: This class is designed for  the methods to sort , print, and search the contacts.
  */
 
-public class ContactList {
+public class contactList {
+
+    private ArrayList<Contact> contactList;
+    
+  /**
+   * This method defines a constructor that creates a new ArrayList when a new object of class contactList is created
+   */
+    public contactList(){
+    	contactList = new ArrayList<Contact>();
+
+    }
+    /**
+     * This method returns a contact
+     * @param index
+     * @return
+     */
+    public Contact getContact(int index){
+    	return contactList.get(index);
+    }
+    /**
+     * This method searches the contact list by email and returns an individual contact.
+     * @return
+     */
+//   public List searchByEmail(){
+//        return searchResultsByEmail;
+//    }
+
+    /**
+     * This method searches the contact list by last name and returns an individual contact.
+     * @return
+     */
+//    public List searchByLastName(){
+//        return searchResultsByLastName;
+//    }
+
+    /**
+     * This method searches the contact list by zip code and returns an individual contact.
+     * @return
+     */
+//    public List searchByZipCode(){
+//        return searchResultsByZipCode;
+//    }
+
+    /**
+     * This method sorts the entire contact list by last name.
+     * @return
+     */
+//    public List sortContactList(){
+//        return sortedContactList;
+//    }
+
+    /**
+     * This method returns the  contact
+     * @return
+     */
+    public String toString(){
+    	String output = "";
+    	for(int i = 0; i<contactList.size();i++){	
+    		output += (getContact(i)+ "\n" + "\n");
+    	}
+        return output;
+    }
+
 }
+/**
+* addperosn
+* printperson
+* contact: print single contact
+* contactlist: printlist of contact
+*/
